2cc8194905a8eaef259afbc470b8c66d16e2dfb0
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-04-30 21:30:32 +0300

    Update TABANAYUVARLAMA.cpp

--- TABANAYUVARLAMA/TABANAYUVARLAMA/TABANAYUVARLAMA.cpp
+++ TABANAYUVARLAMA/TABANAYUVARLAMA/TABANAYUVARLAMA.cpp
@@ -4,11 +4,12 @@
 #include "stdafx.h"
 #include "stdio.h"
 #include "stdlib.h"
-
-
+#include "math.h"
+int TABANAYUVARLAMA(float sayi,int deger);
 int main()
 {
 
+	/*
 	int sum;
 	float money;
 
@@ -22,7 +23,70 @@ int main()
 	printf("\nThe variables you entered were\n");
 	printf("value of sum = %d\n", sum);
 	printf("value of money = %f\n", money);
+	int yakindeger = TABANAYUVARLAMA(money,sum);
+	printf("\n TABANAYUVARLAMA degeri %d \n",yakindeger);
+	*/
+
+
+	int  B[4][4],C[2][4];
+
+	for (size_t i = 0; i <4; i++)
+	{
+		for (size_t j = 0; j <4; j++)
+		{
+			printf(" B [%d][%d] : ",i,j);
+			scanf("%d", &B[i][j]);
+		}
+	}
+
+
+	for (size_t i = 0; i <4; i++)
+	{
+		for (size_t j = 0; j <4; j++)
+		{
+			printf(" %d ",B[i][j]);
+		}
+		printf(" \n ");
+	}
+
+
+
+	for (size_t i = 0; i <4; i++)
+	{
+		for (size_t j = 0; j <4; j++)
+		{
+			if (i)
+			{
+
+			}
+		}
+		printf(" \n ");
+	}
+
+
+
+
 	system("pause");
     return 0;
 }
 
+int TABANAYUVARLAMA(float sayi, int deger)
+{
+	int temp = 0;
+	for (size_t i = 1; i <= sayi; i++)
+	{
+		
+		if (deger<sayi)
+		{
+			temp = deger;
+		}
+		else
+		{
+			break;
+		}
+		deger = deger * temp;
+
+	}
+
+	return temp;
+}
